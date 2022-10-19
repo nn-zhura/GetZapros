3c2cf0f4f9297eabc379fa6781c51bf1b190fd90
MrArtga <artga03@gmail.com>
2022-10-19 13:28:55 +0300

    Гет запрос

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,18 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestTest {
+    @Test
+    public void getUsers() {
+        given()
+                .baseUri("https://reqres.in/api")
+                .basePath("/users")
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200)
+                .body("data[0].email", equalTo("george.bluth@reqres.in"));
+    }
+}
